73aa05f76939d7665c3274202ea53f5c521190e6
Lauri Iivarinen <lauri.iivarinen@gmail.com>
2025-12-15 22:56:06 +0200

    type

---
+++ frontend/src/components/types/DataListingProps.ts
@@ -0,0 +1,8 @@
+import { historyData } from "../../util/types/historyData";
+
+export interface DataListingProps {
+    values: historyData,
+    width?: number,
+    height?: number
+    chartWidthMultiplier?: number
+}
